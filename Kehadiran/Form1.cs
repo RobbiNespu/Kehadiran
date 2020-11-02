50dc3e165dd800b90f34132ccbb6ca3b5c051093
Robbi Nespu <robbi.mohamad@ssi-schaefer.com>
2020-11-02 10:10:06 +0800

    Add project files.

---
+++ Kehadiran/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kehadiran
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetMyCustomFormat();
+        }
+
+        private void SetMyCustomFormat()
+        {
+            //throw new NotImplementedException();
+            // Set the Format type and the CustomFormat string.
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy - dddd";
+        }
+    }
+
+
+}
